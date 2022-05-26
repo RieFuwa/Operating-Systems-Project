4b2a8c6dc1a38a753bf53038d964752ef36e6168
Bedirhan Kabataş <71210936+RieFuwa@users.noreply.github.com>
2022-05-26 23:19:41 +0300

    Add files via upload

--- isletim_sistemleri_proje.cpp
+++ isletim_sistemleri_proje.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 #include <time.h>    
+#include <thread>
+#include "schedule.h"
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
 using namespace std;
 
 
 void sistem_cagrisi(){
     cout<< "Sistem cagrisi yapiliyor..." << endl;
+    Sleep(1000);
+    cout<< "Sistem cagrisi yapiliyor.../bitti" << endl;
 }
 
 void sureci_baslat(){
     cout<< "Surec basliyor..." << endl;
+    Sleep(1000);
+    cout<< "Surec basliyor... /bitti" << endl;
 }
 
 void sureci_bitir(){
     cout<< "Surec sonlaniyor..." << endl;
+    Sleep(1000);
+    cout<< "Surec sonlaniyor.../bitti" << endl;
 }
 
 void olaylari_cagir(int SECIM){
@@ -31,16 +44,24 @@ void olaylari_cagir(int SECIM){
 
 int main(){
     srand (time(NULL));
-    const int OLAY_SAYISI = 7;
     int OLAYLAR[OLAY_SAYISI];
+    thread THREADS[OLAY_SAYISI];
 
     for(int i=0; i<OLAY_SAYISI; i++){
         OLAYLAR[i] = rand()%3;
     }
-    
+
     for(int i=0; i<OLAY_SAYISI; i++){
-        cout<< "Olay : " << OLAYLAR[i] << endl;
-        olaylari_cagir(OLAYLAR[i]);
+        THREADS[i] = thread(olaylari_cagir,OLAYLAR[i]);
+        THREADS[i].join();
     }
+
+    
+
+    int OLAY_SURESI[] = {5, 9, 6, 8, 12, 4, 3};
+ 
+    int OLAY_GELIS_ZAMANI[] = {0, 3, 6, 7, 10, 11, 13};
+ 
+    olayortalamaSureBul(OLAYLAR, OLAY_SAYISI, OLAY_SURESI, OLAY_GELIS_ZAMANI);
 
 }
---
+++ isletim_sistemleri_proje.h
@@ -0,0 +1,54 @@
+
+#include<iostream>
+#define OLAY_SAYISI 3
+
+using namespace std;
+ 
+void olaybeklemeSuresiBul(int OLAYLAR[], int n, int OLAY_SURESI[],
+                                   int OLAY_BEKLEME_SURESI[], int OLAY_GELIS_ZAMANI[])
+{
+    int OLAY_BASLAMA_ZAMANI[OLAY_SAYISI];
+    OLAY_BASLAMA_ZAMANI[0] = OLAY_GELIS_ZAMANI[0];
+    OLAY_BEKLEME_SURESI[0] = 0;
+
+    for (int i = 1; i < n ; i++)
+    {
+        OLAY_BASLAMA_ZAMANI[i] = OLAY_BASLAMA_ZAMANI[i-1] + OLAY_SURESI[i-1];
+        OLAY_BEKLEME_SURESI[i] = OLAY_BASLAMA_ZAMANI[i] - OLAY_GELIS_ZAMANI[i];
+    }
+}
+
+void olaygeridonusSuresiBul(int OLAYLAR[], int n, int OLAY_SURESI[],
+                                      int OLAY_BEKLEME_SURESI[], int OLAY_GERI_DONUS_SURESI[])
+{
+    for (int i = 0; i < n ; i++)
+        OLAY_GERI_DONUS_SURESI[i] = OLAY_SURESI[i] + OLAY_BEKLEME_SURESI[i];
+}
+ 
+void olayortalamaSureBul(int OLAYLAR[], int n, int OLAY_SURESI[], int OLAY_GELIS_ZAMANI[])
+{
+    int OLAY_BEKLEME_SURESI[OLAY_SAYISI], OLAY_GERI_DONUS_SURESI[OLAY_SAYISI];
+    olaybeklemeSuresiBul(OLAYLAR, n, OLAY_SURESI, OLAY_BEKLEME_SURESI, OLAY_GELIS_ZAMANI);
+    olaygeridonusSuresiBul(OLAYLAR, n, OLAY_SURESI, OLAY_BEKLEME_SURESI, OLAY_GERI_DONUS_SURESI);
+
+    cout << "===Olaylar=== " << " ===Olay Suresi=== " << " ===Olay Gelis Zamani=== "
+         << " ===Olay Bekleme Suresi=== " << " ===Olay Donme Suresi=== "
+         << " ===Toplam Sure=== \n";
+         
+    int TOPLAM_BEKLEME_SURESI = 0, TOPLAM_GERI_DONUS_SURESI = 0;
+    for (int i = 0 ; i < n ; i++)
+    {
+        TOPLAM_BEKLEME_SURESI = TOPLAM_BEKLEME_SURESI + OLAY_BEKLEME_SURESI[i];
+        TOPLAM_GERI_DONUS_SURESI = TOPLAM_GERI_DONUS_SURESI + OLAY_GERI_DONUS_SURESI[i];
+        int TOPLAM_SURE = OLAY_GERI_DONUS_SURESI[i] + OLAY_GELIS_ZAMANI[i];
+        cout << " " << i+1 << "\t\t" << OLAY_SURESI[i] << "\t\t\t"
+             << OLAY_GELIS_ZAMANI[i] << "\t\t\t" << OLAY_BEKLEME_SURESI[i] << "\t\t\t "
+             << OLAY_GERI_DONUS_SURESI[i]  <<  "\t\t\t " << TOPLAM_SURE << endl;
+    }
+ 
+ cout << "\nOrtalama Donme Suresi = "
+         << (float)TOPLAM_GERI_DONUS_SURESI / (float)n;
+    cout << "Ortalama Bekleme Suresi = "
+         << (float)TOPLAM_BEKLEME_SURESI / (float)n;
+    
+}
